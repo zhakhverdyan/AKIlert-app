226ec2a0a24f2840f41a6ec467e13489c9309e25
Zhanna Hakhverdyan <zhakhverdyan@gmail.com>
2020-06-11 14:01:35 -0400

    app working

--- app.py
+++ app.py
@@ -6,16 +6,28 @@ import pickle
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('index.html')
-
 def get_prediction(num):
     df=pd.read_csv('df_sample.csv')
-    final_features = df.iloc[num-1].values
+    final_features = df.iloc[num-1].values.reshape(1,-1)
     prediction = np.squeeze(model.predict_proba(final_features))
     output = round(prediction[1], 3)
     return output
+
+
+@app.route('/', methods=['GET', 'POST'])
+def predict():
+    if request.method == "POST":
+        print('posted')
+        num=int(request.form.get("patient"))
+        return render_template('index.html', prediction_text='Patient risk is {}'.format(get_prediction(num)))
+    else:
+        return render_template('index.html')
+
+"""
+def home():
+    return render_template('index.html')
+
+
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
@@ -24,7 +36,7 @@ def predict():
         return render_template('index.html', prediction_text='Patient risk is {}'.format(get_prediction(num)))
     else:
         return render_template('index.html')
-"""
+
 @app.route('/results', methods=['GET', 'POST'])
 def results():
     if request.method == "POST":
