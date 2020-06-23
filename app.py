7dbbf9c10fcd0f10aef86d76119da3a3a1de92f0
Zhanna Hakhverdyan <zhakhverdyan@gmail.com>
2020-06-23 04:06:31 -0400

    new app, graph with time series data

--- app.py
+++ app.py
@@ -3,38 +3,50 @@ import pandas as pd
 import json
 import plotly
 from flask import Flask, request, jsonify, render_template
-import pickle
+#import pickle
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+#model = pickle.load(open('model.pkl', 'rb'))
 
-df_var = pd.read_csv('csv_files/var_subset.csv')
+#df_var = pd.read_csv('csv_files/var_subset.csv')
+df_data = pd.read_csv('csv_files/for_app.csv')
 
+"""
 def get_prediction(num):
     df=pd.read_csv('csv_files/df_sample.csv')
     final_features = df.iloc[num-1].values.reshape(1,-1)
     prediction = np.squeeze(model.predict_proba(final_features))
     output = round(prediction[1], 3)
     adjusted_prob = output*0.5/0.03
-    return round(adjusted_prob, 2)
+    return round(adjusted_prob, 2)"""
 
-def plot_figure(num):
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df_var[df_var['aki_label']==0]['min_result_calcium'],\
-                            y=df_var[df_var['aki_label']==0]['max_result_creatinine'], \
-                        mode='markers',
-                        name='negative class'))
-    fig.add_trace(go.Scatter(x=df_var[df_var['aki_label']==1]['min_result_calcium'],\
-                            y=df_var[df_var['aki_label']==1]['max_result_creatinine'], \
-                        mode='markers',
-                        name='positive class'))
-    fig.add_trace(go.Scatter(x=[5], y=[30],
-                        mode='markers',
-                        name='hypothetical patient'))
-    fig.update_layout(title='Creatinine and calcium levels by patient class',
-                    xaxis_title="Minimum Calcium level (mg/dL)",
-                    yaxis_title="Maximum Creatinine level (mg/dL)",)
+def plot_figure(num, df_data):
+    
+    df = df_data[df_data['patient']==num]
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    #fig = go.Figure()
+    x=df['Lab measurement day'].values
+    fig.add_trace(go.Scatter(
+        x=df['Lab measurement day'].values,
+        y=df['Normalized score'].values,
+        name='Probability',
+        mode='lines'),
+        secondary_y=False)
+
+
+    fig.add_trace(
+        go.Scatter(x=df['Lab measurement day'], y=df['creatinine'], name="Creatinine"),
+        secondary_y=True)
+
+    fig.update_layout(
+        title = 'Probability of acute kidney injury in the next 12-36h',
+        xaxis_title="Days in relation to ICU admission")
+
+    fig.update_yaxes(title_text="Normalized probability",range=[0,1], secondary_y=False)
+    fig.update_yaxes(title_text="Creatinine (mg/dL)", secondary_y=True)
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
 
@@ -44,10 +56,10 @@ def predict():
     if request.method == "POST":
         print('posted')
         num=int(request.form.get("patient"))
-        graphJSON=plot_figure(num)
+        graphJSON=plot_figure(num, df_data)
         patient = request.form.get('patient', '')
-        print(patient)
-        return render_template('index.html', patient = patient, prediction_text='Patient risk is {}'.format(get_prediction(num)), graphJSON=graphJSON)
+        return render_template('index.html', patient = patient, graphJSON=graphJSON)
+        #return render_template('index.html', patient = patient, prediction_text='Patient risk is {}'.format(get_prediction(num)), graphJSON=graphJSON)
     else:
         return render_template('index.html')
 
